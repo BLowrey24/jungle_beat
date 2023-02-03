dbb494da5b838a60fecd3f7b6d68a4bc5d241870
Boston Lowrey <bostonlowrey@gmail.com>
2023-02-03 14:59:23 -0500

    add test: for linkedlist #initialize

---
+++ spec/linked_list_spec.rb
@@ -0,0 +1,17 @@
+require "./lib/node"
+require "./lib/linked_list"
+
+RSpec.describe Node do
+  let(:list) { LinkedList.new }
+  let(:node) { Node.new("plop") }
+
+  describe "#initialize" do
+    it 'exists' do
+      expect(list).to be_an_instance_of(LinkedList)
+    end
+
+    it 'has attributes' do
+      expect(list.head).to eq(nil)
+    end
+  end
+end
